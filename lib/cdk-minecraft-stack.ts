0ea9167ed4089677de68fc09799fb8b3929909e0
Dave Burke <dburke84@gmail.com>
2020-12-26 20:42:45 -0600

    Add EFS filesystem

--- lib/cdk-minecraft-stack.ts
+++ lib/cdk-minecraft-stack.ts
@@ -1,10 +1,24 @@
 import * as cdk from '@aws-cdk/core';
 import { Vpc } from '@aws-cdk/aws-ec2'
+import * as efs from '@aws-cdk/aws-efs'
+import { LifecyclePolicy } from '@aws-cdk/aws-efs';
+import { RemovalPolicy } from '@aws-cdk/core';
+
+const test = true;
 
 export class CdkMinecraftStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const vpc = new Vpc(this, 'MinecraftVpc')
+
+    const fileSystem = new efs.FileSystem(this, 'MinecraftEfs', {
+      vpc,
+      encrypted: true,
+      enableAutomaticBackups: !test,
+      lifecyclePolicy: LifecyclePolicy.AFTER_7_DAYS,
+      removalPolicy: test ? RemovalPolicy.DESTROY : RemovalPolicy.RETAIN,
+    })
+
   }
 }
