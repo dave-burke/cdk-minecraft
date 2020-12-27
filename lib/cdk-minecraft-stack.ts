97e0035fdd9d12880ffbe0050bacd18da46dfe8f
Dave Burke <dburke84@gmail.com>
2020-12-27 10:31:43 -0600

    Use ECS instead of EC2 directly

--- lib/cdk-minecraft-stack.ts
+++ lib/cdk-minecraft-stack.ts
@@ -5,54 +5,65 @@ import { LifecyclePolicy } from '@aws-cdk/aws-efs'
 import { RemovalPolicy } from '@aws-cdk/core'
 import { InstanceType, MachineImage } from '@aws-cdk/aws-ec2'
 import { AutoScalingGroup, ScheduledAction, Schedule } from '@aws-cdk/aws-autoscaling'
+import * as ecs from '@aws-cdk/aws-ecs'
 
 const TEST = true
 const INSTANCE_TYPE = 't4g.medium'
+const SPOT_PRICE = '0.0336'
 
 export class CdkMinecraftStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    const vpc = new Vpc(this, 'MinecraftVpc')
+    const cluster = new ecs.Cluster(this, 'MinecraftCluster')
+    cluster.connections.allowFromAnyIpv4(Port.tcp(22565))
 
-    const server = new AutoScalingGroup(this, 'MinecraftServer', {
-      vpc,
+    const autoScalingGroup = cluster.addCapacity('MinecraftServer', {
       instanceType: new InstanceType(INSTANCE_TYPE),
-      machineImage: MachineImage.latestAmazonLinux(),
+      machineImage: ecs.EcsOptimizedImage.amazonLinux2(),
       desiredCapacity: 1,
+      spotPrice: SPOT_PRICE,
     })
-    server.connections.allowFromAnyIpv4(Port.tcp(22565))
 
     new ScheduledAction(this, 'ScaleDownMinecraft', {
-      autoScalingGroup: server,
+      autoScalingGroup,
       schedule: Schedule.cron({ hour: '22', minute: '0' }),
       desiredCapacity: 0
     })
     new ScheduledAction(this, 'ScaleUpMinecraft', {
-      autoScalingGroup: server,
+      autoScalingGroup,
       schedule: Schedule.cron({ hour: '15', minute: '0' }),
       desiredCapacity: 1
     })
 
     const fileSystem = new efs.FileSystem(this, 'MinecraftEfs', {
-      vpc,
+      vpc: cluster.vpc,
       encrypted: true,
       enableAutomaticBackups: !TEST,
       lifecyclePolicy: LifecyclePolicy.AFTER_7_DAYS,
       removalPolicy: TEST? RemovalPolicy.DESTROY : RemovalPolicy.RETAIN,
     })
 
-    fileSystem.connections.allowDefaultPortFrom(server)
+    const ec2Task = new ecs.Ec2TaskDefinition(this, 'MinecraftTask')
+    ec2Task.addContainer('MinecraftServerContainer', {
+      image: ecs.ContainerImage.fromRegistry('itzg/minecraft:latest'),
+      memoryLimitMiB: 3584,
+      environment: {
+        'EULA': 'true',
+        'DIFFICULTY': 'normal',
+      }
+    })
+    ec2Task.addVolume({
+      name: 'MinecraftEfsVolume',
+      efsVolumeConfiguration: {
+        fileSystemId: fileSystem.fileSystemId,
+        rootDirectory: '/opt/minecraft'
+      }
+    })
 
-    server.userData.addCommands("yum check-update -y",
-      "yum upgrade -y",
-      "yum install -y amazon-efs-utils",
-      "yum install -y nfs-utils",
-      "file_system_id_1=" + fileSystem.fileSystemId,
-      "efs_mount_point_1=/mnt/efs/fs1",
-      "mkdir -p \"${efs_mount_point_1}\"",
-      "test -f \"/sbin/mount.efs\" && echo \"${file_system_id_1}:/ ${efs_mount_point_1} efs defaults,_netdev\" >> /etc/fstab || " +
-      "echo \"${file_system_id_1}.efs." + this.region + ".amazonaws.com:/ ${efs_mount_point_1} nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,_netdev 0 0\" >> /etc/fstab",
-      "mount -a -t efs,nfs4 defaults");
+    const service = new ecs.Ec2Service(this, 'MinecraftService', {
+      cluster,
+      taskDefinition: ec2Task,
+    })
   }
 }
