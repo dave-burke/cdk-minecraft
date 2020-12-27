7a84b851a21fd52527ecc5bd6fd9d22456b62698
Dave Burke <dburke84@gmail.com>
2020-12-26 21:31:24 -0600

    Add EC2 instance

--- lib/cdk-minecraft-stack.ts
+++ lib/cdk-minecraft-stack.ts
@@ -1,31 +1,45 @@
 import * as cdk from '@aws-cdk/core'
-import { Vpc } from '@aws-cdk/aws-ec2'
 import * as efs from '@aws-cdk/aws-efs'
 import * as ec2 from '@aws-cdk/aws-ec2'
 import { LifecyclePolicy } from '@aws-cdk/aws-efs'
 import { RemovalPolicy } from '@aws-cdk/core'
+import { InstanceType, MachineImage } from '@aws-cdk/aws-ec2'
 
-const test = true
+const TEST = true
+const INSTANCE_TYPE = 't4g.medium'
 
 export class CdkMinecraftStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
-    const vpc = new Vpc(this, 'MinecraftVpc')
+    const vpc = new ec2.Vpc(this, 'MinecraftVpc')
 
-    const efsSecurityGroup = new ec2.SecurityGroup(this, 'EfsSecurityGroup', {
+    const server = new ec2.Instance(this, 'MinecraftServer', {
       vpc,
-      allowAllOutbound: true,
+      instanceType: new InstanceType(INSTANCE_TYPE),
+      machineImage: MachineImage.latestAmazonLinux(),
     })
+    server.connections.allowFromAnyIpv4(ec2.Port.tcp(22565))
 
     const fileSystem = new efs.FileSystem(this, 'MinecraftEfs', {
       vpc,
       encrypted: true,
-      enableAutomaticBackups: !test,
+      enableAutomaticBackups: !TEST,
       lifecyclePolicy: LifecyclePolicy.AFTER_7_DAYS,
-      removalPolicy: test ? RemovalPolicy.DESTROY : RemovalPolicy.RETAIN,
-      securityGroup: efsSecurityGroup,
+      removalPolicy: TEST? RemovalPolicy.DESTROY : RemovalPolicy.RETAIN,
     })
 
+    fileSystem.connections.allowDefaultPortFrom(server)
+
+    server.userData.addCommands("yum check-update -y",
+      "yum upgrade -y",
+      "yum install -y amazon-efs-utils",
+      "yum install -y nfs-utils",
+      "file_system_id_1=" + fileSystem.fileSystemId,
+      "efs_mount_point_1=/mnt/efs/fs1",
+      "mkdir -p \"${efs_mount_point_1}\"",
+      "test -f \"/sbin/mount.efs\" && echo \"${file_system_id_1}:/ ${efs_mount_point_1} efs defaults,_netdev\" >> /etc/fstab || " +
+      "echo \"${file_system_id_1}.efs." + this.region + ".amazonaws.com:/ ${efs_mount_point_1} nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport,_netdev 0 0\" >> /etc/fstab",
+      "mount -a -t efs,nfs4 defaults");
   }
 }
