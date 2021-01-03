0359355c0c362f01ab874ada9b976c2d3c1530b3
Dave Burke <dburke84@gmail.com>
2021-01-03 09:32:47 -0600

    Parameterize config values

--- lib/cdk-minecraft-stack.ts
+++ lib/cdk-minecraft-stack.ts
@@ -9,26 +9,29 @@ import * as lambda from '@aws-cdk/aws-lambda'
 import * as path from 'path'
 import * as targets from '@aws-cdk/aws-events-targets'
 import { CdkMinecraftSpotPricing, CdkMinecraftSpotPricingProps, CdkMinecraftSpotPricingDnsConfig } from './cdk-minecraft-spot-pricing'
+import * as dotenv from 'dotenv'
 
-const TEST = true
-const TIMEZONE_OFFSET = -6 // CST
+dotenv.config()
+
+const DEBUG: boolean = process.env.DEBUG ? Boolean(process.env.DEBUG) : false 
+const TIMEZONE_OFFSET: number = Number(process.env.TIMEZONE_OFFSET) ?? 0
+const CONTAINER_ENV: string = process.env.CONTAINER_ENV ? JSON.parse(process.env.CONTAINER_ENV) : {}
+const HOSTED_ZONE_ID: string = process.env.HOSTED_ZONE_ID ?? ''
+const DNS_RECORD_NAME: string = process.env.DNS_RECORD_NAME ?? ''
 
 export class CdkMinecraftStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
 
     const server = new CdkMinecraftSpotPricing(this, 'MinecraftServer', {
-      spotPrice: '0.0416',
-      enableAutomaticBackups: !TEST,
-      efsRemovalPolicy: TEST ? cdk.RemovalPolicy.DESTROY : cdk.RemovalPolicy.RETAIN,
+      spotPrice: process.env.SPOT_PRICE,
+      enableAutomaticBackups: !DEBUG,
+      efsRemovalPolicy: DEBUG ? cdk.RemovalPolicy.DESTROY : cdk.RemovalPolicy.RETAIN,
       dnsConfig: {
-        hostedZoneId: '[hosted zone ID from Route53]',
-        recordName: 'minecraft.example.com',
+        hostedZoneId: HOSTED_ZONE_ID,
+        recordName: DNS_RECORD_NAME,
       },
-      containerEnvironment: {
-        'EULA': 'true',
-        'DIFFICULTY': 'normal',
-      },
+      containerEnvironment: CONTAINER_ENV,
     })
 
     new autoscaling.ScheduledAction(this, 'ScaleDown', {
