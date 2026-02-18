4bcb57165a1b9655087b9789ae782a8cc98f533c
Dave Burke <dburke84@gmail.com>
2026-02-17 19:03:26 -0600

    Tag stacks

--- bin/cdk-minecraft.ts
+++ bin/cdk-minecraft.ts
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
 import "source-map-support/register";
-import { App } from "aws-cdk-lib";
+import { App, Tags } from "aws-cdk-lib";
 import { CdkMinecraftStack } from "../lib/cdk-minecraft-stack";
 import "dotenv/config";
 
 const app = new App();
-new CdkMinecraftStack(app, "CdkMinecraftStack", {
+const stack = new CdkMinecraftStack(app, "CdkMinecraftStack", {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
   },
 });
+
+Tags.of(stack).add("project", "minecraft-spot-pricing");
+Tags.of(stack).add("iac", "cdk");
